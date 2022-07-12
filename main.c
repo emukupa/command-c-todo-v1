e288f51330bf63f6b1f6d36948a3a4ef8f61eaf7
emukupa1 <edward-manda@uhatok.com>
2022-07-11 22:16:29 -0500

    define the basic crud operations

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "todo.h"
 
 int main(int argc, char const *argv[])
 {
--- todo.h
+++ todo.h
@@ -1,4 +1,15 @@
 #ifndef TODO_H
 #define TODO_H
 
+// CreateReadUpdateDelete
+// extern int updateTodoTasks(int id);
+extern int completeTodoTask(int task_id);
+
+extern int createTodoTask(char *todoTask);
+
+extern int deletedTodoTask(int id);
+
+// void readTodoTasks();
+void loadTodoTasks();
+
 #endif // !TODO_H
